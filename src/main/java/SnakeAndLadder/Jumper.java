1e4d6162e8a1f3dfbb26a5dac78ccf73a2b050b0
Harsh Jain <harshj12988@gmail.com>
2021-08-21 13:28:03 +0530

    Implemented Snake and Ladder using oops concepts.

---
+++ src/main/java/SnakeAndLadder/Jumper.java
@@ -0,0 +1,28 @@
+package SnakeAndLadder;
+
+
+public class Jumper {
+    int startPoint;
+    int endPoint;
+
+    public int getStartPoint() {
+        return startPoint;
+    }
+
+    public void setStartPoint(int startPoint) {
+        this.startPoint = startPoint;
+    }
+
+    public int getEndPoint() {
+        return endPoint;
+    }
+
+    public void setEndPoint(int endPoint) {
+        this.endPoint = endPoint;
+    }
+
+    public Jumper(int startPoint, int endPoint) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+    }
+}
